dad4876c7d63ea32578009e4e9317daca58d50a3
MattiasOz <mattiasjosefoz@gmail.com>
2024-02-20 14:37:52 +0100

    Bug fix + label files

--- label_maker.py
+++ label_maker.py
@@ -8,17 +8,37 @@ from pprint import pprint
 
 import os
 
-
-if __name__ == '__main__':
-    path = './skogsstyrelsen-data/'
-    train = np.load(path + 'skogs_json_val.npy', allow_pickle=True)
+def train(path):
+    train = np.load(path + 'skogs_json_train.npy', allow_pickle=True)
     answers = {}
     for t in train:
-        answers[t['ValideringsobjektBildId']] = t['MolnDis']
+        answers['skgs_'+t['ValideringsobjektBildId']+'.nc'] = t['MolnDis']
     
-    #with open('val_labels.pkl', 'wb') as f:
-    #    pickle.dump(answers, f)
+    with open('train_labels.pkl', 'wb') as f:
+        pickle.dump(answers, f)
+    
+    with open('train_labels.pkl', 'rb') as f:
+        thing = pickle.load(f)
+    pprint(thing)
+
+def val(path):
+    validation = np.load(path + 'skogs_json_val.npy', allow_pickle=True)
+    answers = {}
+    for v in validation:
+        answers['skgs_'+v['ValideringsobjektBildId']+'.nc'] = v['MolnDis']
+
+    with open('val_labels.pkl', 'wb') as f:
+        pickle.dump(answers, f)
+    
 
     with open('val_labels.pkl', 'rb') as f:
         thing = pickle.load(f)
     pprint(thing)
+
+
+if __name__ == '__main__':
+    path = './skogsstyrelsen-data/'
+
+    train(path)
+
+    val(path)
